14f42ff5e1a16aa63a017def801d5c0baf4c3115
Devin Brown <devin.g.brown@gmail.com>
2013-01-08 22:14:15 -0700

    removed code in generated that is not needed (I think), no ill effects - mission achieved

--- lib/generators/rails/my_helper/my_helper_generator.rb
+++ lib/generators/rails/my_helper/my_helper_generator.rb
@@ -1,10 +1,6 @@
 class Rails::MyHelperGenerator < Rails::Generators::NamedBase
   def create_helper_file
-    create_file "app/helpers/#{file_name}_helper.rb", <<-FILE
-module #{class_name}Helper
-  attr_reader :#{plural_name}, :#{plural_name.singularize}
-end
-    FILE
+    create_file "app/helpers/#{file_name}_helper.rb"
   end
   hook_for :test_framework, :as => :helper
 end
